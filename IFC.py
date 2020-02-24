a9739855931ae3f9ba0ee6f2556a9b2724a33937
chenhsi <sungchenhsi@gmail.com>
2020-02-24 23:58:33 +0000

    change code

--- IFC.py
+++ IFC.py
@@ -61,18 +61,7 @@ class ParseIFC:
                 if len(contain) == 0: continue
                 item =  contain[0].RelatingStructure.Name
 
-                ##for For Gravens_Hill_260719 second case
-                # if self.__targetLevel == "00 Ground Floor" and item == "01 First Floor" and element.GlobalId == "3hR3mfu7LA8ed1jTcubv7L":
-                #     SaveGUID.append(element.GlobalId)
-                # elif self.__targetLevel == "01 First Floor" and item == "02 Roof Plan" and element.GlobalId == "0s8adBwwP3wO0g27aFJZfR":
-                #     SaveGUID.append(element.GlobalId)
-                # elif self.__targetLevel == "01 First Floor" and item == "02 Roof Plan" and element.GlobalId == "0s8adBwwP3wO0g27aFJZeb":
-                #     SaveGUID.append(element.GlobalId)
-                # else:
-                #     if item != self.__targetLevel:
-                #         ifcFile.remove(element)
-                #     else:
-                #         SaveGUID.append(element.GlobalId)
+                #for For Gravens_Hill_260719 second case
 
                 if item != self.__targetLevel:
                     print(element.Name)
