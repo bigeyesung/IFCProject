d153a432a1e28e19e436cf60ad4bea10cc125b3b
chenhsi <sungchenhsi@gmail.com>
2020-03-22 22:07:23 +0000

    code change for wrapper funcs

--- multiprocess.py
+++ multiprocess.py
@@ -2,6 +2,19 @@ from numba import jit
 import numpy as np
 import time
 
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
+def say_whee():
+    print("Whee!")
+say_whee()
+
+print("ddon")
 SQRT_2PI = np.sqrt(2 * np.pi)
 
 @jit(nopython=True, parallel=True)
@@ -18,6 +31,15 @@ def gaussians(x, means, widths):
 
 means = np.random.uniform(-1, 1, size=1000000)
 widths = np.random.uniform(0.1, 0.3, size=1000000)
+
+
+# normal case
+start = time.time()
+gaussians_nothread = jit(nopython=True, parallel = False)(gaussians.py_func)
+gaussians_nothread(0.4, means, widths)
+end = time.time()
+print("Elapsed normal case = %s" % (end - start))
+
 # DO NOT REPORT THIS... COMPILATION TIME IS INCLUDED IN THE EXECUTION TIME!
 start = time.time()
 gaussians(0.4, means, widths)
